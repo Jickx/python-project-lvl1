43539989d681dad3fe738cbdffa6e362c60367ce
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-21 16:37:14 +0300

    add brain-even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import prompt
+from random import randrange
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string("May I have your name? ")
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    ctr = 0
+    while True:
+        if ctr == 3:
+            print(f'Congratulations, {name}!')
+            break
+        number = randrange(100)
+        correct_answer = 'yes' if number % 2 == 0 else 'no'
+        print(f'Question: {number}')
+        answer = prompt.string('Your answer: ')
+        if answer.lower() == correct_answer:
+            print('Correct!')
+            ctr += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer "
+                  f"was '{correct_answer}'.")
+            print(f'Let\'s try again, {name}!')
+            ctr = 0
+            break
+
+
+if __name__ == '__main__':
+    main()
