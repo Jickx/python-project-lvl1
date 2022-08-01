bef01e1b464f3647cccb14307483d0668c8d2f21
Jickx <63093696+Jickx@users.noreply.github.com>
2022-08-01 19:32:04 +0300

    add shebangs

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import prompt
 from random import randrange
 from brain_games.games.interactions import welcome_user
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from brain_games.games.interactions import (
     welcome_user,
     success,
