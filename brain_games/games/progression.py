2ae07de579f4d0027bcd3a3103717ae0943c812b
Jickx <63093696+Jickx@users.noreply.github.com>
2022-08-01 19:06:49 +0300

    add progression

---
+++ brain_games/games/progression.py
@@ -0,0 +1,23 @@
+import prompt
+from random import randrange
+
+
+def generate_progression() -> list[int]:
+    prog_diff = randrange(2, 4)
+    prog_length = randrange(5, 10)
+    prog_start = randrange(5, 50)
+    prog_end = prog_start + prog_diff * prog_length
+    answer_index = randrange(0, prog_length)
+    prog = [i for i in range(prog_start, prog_end + 1, prog_diff)]
+    correct_answer = prog[answer_index]
+    prog[answer_index] = '..'
+    prog_str = ' '.join(str(i) for i in prog)
+    return prog_str, correct_answer
+
+
+def ask_question() -> int:
+    prog_str, correct_answer = generate_progression()
+    print('What number is missing in the progression?')
+    print(f'Question: {prog_str}')
+    answer = prompt.integer(prompt='Your answer: ')
+    return answer, correct_answer
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,17 @@
+from brain_games.games.interactions import (
+    welcome_user,
+    success,
+    failure,
+    is_right_answer
+)
+from brain_games.games.progression import ask_question
+
+
+def main():
+    name = welcome_user()
+    for i in range(3):
+        answer, correct_answer = ask_question()
+        if not is_right_answer(answer, correct_answer):
+            failure(name)
+            return None
+    success(name)
