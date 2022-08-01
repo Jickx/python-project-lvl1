3600c4fae58204a910425d50996da111785dcad9
Jickx <63093696+Jickx@users.noreply.github.com>
2022-08-01 20:54:59 +0300

    add prime

---
+++ brain_games/games/prime.py
@@ -0,0 +1,20 @@
+from random import randrange
+import prompt
+
+
+def is_prime(num):
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True
+    return False
+
+
+def ask_question():
+    num = randrange(100)
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    print(f'Question: {num}')
+    answer = prompt.string(prompt='Your answer: ')
+    correct_answer = 'yes' if is_prime(num) else 'no'
+    return answer.lower(), correct_answer
