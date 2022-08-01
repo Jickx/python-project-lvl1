28f68728c90b835d97a1cb56250646b8ffe6c0b7
Jickx <63093696+Jickx@users.noreply.github.com>
2022-08-01 22:02:32 +0300

    constatnts

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,7 +1,7 @@
 import prompt
 from random import randrange, choice
 
-operators = {
+OPERATORS = {
     '+': lambda x, y: x + y,
     '-': lambda x, y: x - y,
     '*': lambda x, y: x * y,
@@ -11,9 +11,9 @@ operators = {
 def ask_question() -> int:
     num1 = randrange(10)
     num2 = randrange(10)
-    operator = choice(list(operators.keys()))
+    operator = choice(list(OPERATORS.keys()))
     print('What is the result of the expression?')
     print(f'Question: {num1} {operator} {num2}')
     answer = prompt.integer(prompt=('Your answer: '))
-    correct_answer = operators[operator](num1, num2)
+    correct_answer = OPERATORS[operator](num1, num2)
     return answer, correct_answer
